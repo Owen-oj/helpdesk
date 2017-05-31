b977d32a2979455e04cd2e7d0bc345759f25a190
Owen Jubilant <jubifis@gmail.com>
2017-05-31 06:59:41 +0000

    few changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -53,14 +53,14 @@
 
 
 <div class="col-md-8 ">
-    <div class="col-md-6">
+
         <div class="panel panel-default">
             <div class="panel-heading">Tickets</div>
             <div class="panel-body">
                 <canvas id="agents"></canvas>
             </div>
         </div>
-    </div>
+
    {{-- <div class="col-md-6">
         <div class="panel panel-default">
             <div class="panel-heading">Categories</div>
@@ -182,15 +182,15 @@
     <h2 class="title">Recently Solved Cases</h2>
     <div class="row">
         @foreach($solutions as $solution)
-            <div class="col-md-4">
+            <div class="col-md-6">
                 <div class="panel-group">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             <h4 class="panel-title">
-                                <a data-toggle="collapse" href="#collapse1">{{$solution->ticket->subject}}</a>
+                                <a data-toggle="collapse" href="#collapse{{$solution->id}}">{{$solution->ticket->subject}}</a>
                             </h4>
                         </div>
-                        <div id="collapse1" class="panel-collapse collapse">
+                        <div id="collapse{{$solution->id}}" class="panel-collapse collapse">
                             <div class="panel-body">{!! $solution->solution !!}</div>
                             <div class="panel-footer">Solved By {{$solution->agent->name}}</div>
                         </div>
