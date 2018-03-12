08b2ef8fcf9a8eac98a0f59b2cf78bbba2241bb2
Owen Jubilant <jubifis@gmail.com>
2018-03-12 13:24:14 +0000

    typo

--- gulpfile.js
+++ gulpfile.js
@@ -16,9 +16,9 @@ require('laravel-elixir-vue-2');
 elixir((mix) => {
     //mix.copy('nodule_modules/summernote/dist/summernote.css','public/css/');
     mix.styles([
-            'summernote/dist/summernote.css',
-            'bootstrap-colorpicker/dist/css/bootstrap-colorpicker.css'
-        ], 'public/css/custom.css','node_modules');
+        'summernote/dist/summernote.css',
+        'bootstrap-colorpicker/dist/css/bootstrap-colorpicker.css'
+    ], 'public/css/custom.css', 'node_modules');
     mix.sass('app.scss')
         .webpack('app.js');
 
